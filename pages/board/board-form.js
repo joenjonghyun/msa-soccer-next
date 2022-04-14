5fa7f78d4e61c53b28c7a11d66b81f466819a564
joenjonghyun <jjh980814@naver.com>
2022-04-14 18:15:17 +0900

    22.4.14

--- pages/board/board-form.js
+++ pages/board/board-form.js
@@ -1,60 +1,49 @@
-import React,{useState} from 'react'
-import style from "board/style/board-form.module.css";
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import {addTask} from '../../redux/reducers/board.reducer'
-
-export default function BoardhtmlForm(){
-    const [inputs, setInputs] = useState({})
+import React, {useState} from 'react'
+import style from "board/style/board-form.module.css"
+import { useDispatch } from 'react-redux'
+import { addBoard } from '../../redux/reducers/boardReducer.ts'
+export default function Board(){
     const dispatch = useDispatch()
+    const [inputs, setInputs] = useState({})
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        const {inputs, name} = e.target
-        setInputs({
-            ...inputs,
-            [name] : inputs
-        })
+    const handleChange = e => {
+       const {name, value} = e.target 
+       setInputs({...inputs, [name]: value})
     }
-    const handleSubmit = e => {
-        e.preventDefault()
-        alert('value?' +inputs)
-        if(inputs) dispatch(addTask({task: inputs}))
-        axios.post('http://localhost:5000/api/board/write', inputs)
-        .then(res => {
-            alert(JSON.stringify(res.data))
-        }) 
-        .catch (err => alert(err))
-    }
+
+   
     return (<>
         <h1>게시글 등록</h1>
         <div className={style.container}>
-            <htmlForm action="">
+            <form onSubmit={e => {
+                e.preventDefault()
+                
+                if(inputs) dispatch(addBoard(inputs))
+            }}>
             <div className={style.row}>
-                <div className={style.col-25}>
+                <div className={style.col25}>
                 <label className={style.label} htmlFor="passengerId">글 제목</label>
                 </div>
-                <div className={style.col-75}>
+                <div className={style.col75}>
                 <input type="text" className={style.inputText} onChange={handleChange}
                 id="title" name="title" placeholder="글 제목 입력"/>
                 </div>
             </div>
-            {/**
             <div className={style.row}>
-                <div className={style.col-25}>
+                <div className={style.col25}>
                 <label htmlFor="name">게시글 작성자 이름</label>
                 </div>
-                <div className={style.col-75}>
+                <div className={style.col75}>
                 <input type="text" className={style.inputText} onChange={handleChange}
                 id="name" name="name" placeholder="게시글 작성자 이름 입력"/>
                 </div>
             </div>
             <div className={style.row}>
-                <div className={style.col-25}>
+                <div className={style.col25}>
                 <label htmlFor="team">응원팀</label>
                 </div>
-                <div className={style.col-75}>
-                <select id="teamId" onChange={handleChange} name="teamId">
+                <div className={style.col75}>
+                <select id="teamId" name="teamId" onChange={handleChange}>
                     <option value="">응원팀 선택</option>
                     <option value="K09">Fc seoul</option>
                     <option value="K02">Suwon Samseong blue wings</option>
@@ -63,20 +52,20 @@ export default function BoardhtmlForm(){
                 </div>
             </div>
             <div className={style.row}>
-                <div className={style.col-25}>
+                <div className={style.col25}>
                 <label htmlFor="subject">게시글 내용</label>
                 </div>
-                <div className={style.col-75}>
-                <input type="textarea"  onChange={handleChange} id="subject" name="subject" style={{height:200 + "px"}}></input>
+                <div className={style.col75}>
+                <input type="textarea"  id="subject" name="subject" onChange={handleChange} style={{height:200 + "px"}}></input>
                 </div>
             </div>
             <br/>
-            */}
+            
             <div className={style.row}>
-                <input type="submit" onClick={handleSubmit} className={style.inputSubmit}
+                <input type="submit" className={style.inputSubmit} 
                 value="Submit"/>
             </div>
-            </htmlForm>
+            </form>
             </div>
     </>)
 }
--- redux/api/todoApi.ts
+++ redux/api/todoApi.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios'
 const SERVER = 'http://127.0.0.1:5000'
 const headers = {
     "Content-Type": "application/json",
     Authorization: "JWT fefege...",
 }
 
-const joinAPI = (data) => axios.post(`${SERVER}/api/user/signup`,inputs, { headers})
+const addTask = data => axios.post(`${SERVER}/api/todo/add-task`,data.task, { headers})
 
 export default {
-    joinAPI 
+    addTask
 }
