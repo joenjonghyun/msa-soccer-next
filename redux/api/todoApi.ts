8e3a88a2ce1c06042cce9d2aae8b95d09a1f8353
joenjonghyun <jjh980814@naver.com>
2022-04-18 20:04:47 +0900

    22.4.18.todo해야함

--- redux/api/todoApi.ts
+++ redux/api/todoApi.ts
@@ -1,12 +1,23 @@
-import axios from 'axios'
+import axios,{ AxiosResponse } from 'axios'
+
 const SERVER = 'http://127.0.0.1:5000'
 const headers = {
     "Content-Type": "application/json",
     Authorization: "JWT fefege...",
 }
+export interface TodoType{
+            userid: '';
+            task: ''; 
+            completed: ''
+        }
 
-const addTask = data => axios.post(`${SERVER}/api/todo/add-task`,data.task, { headers})
-
-export default {
-    addTask
-}
+export const postTodo = async (payload:
+     {userid: '', task: '', completed: '' }) => {
+            try{
+                const response : AxiosResponse<unknown, TodoType[]> = await axios.post(`${SERVER}/api/todo/addTodo`,payload, {headers})
+            alert('진행 5 : 응답성공 '+JSON.stringify(response.data))
+            return response.data
+         }catch(err){
+            return err;
+         }
+    }
